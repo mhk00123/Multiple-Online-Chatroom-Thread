3f5fd9012fabdce29112c8189e708f3a1b2b93ae
LiLi.Tam <mhk00123@gmail.com>
2019-08-20 16:53:40 +0800

    v1.0
    1. 建立雙方連接

--- src/lili/com/chatroom/Chat_Server.java
+++ src/lili/com/chatroom/Chat_Server.java
@@ -1,9 +1,25 @@
 package lili.com.chatroom;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * *在線聊天室 : Chat Server 端
  * @author LiLi-PC
  *
  */
 public class Chat_Server {
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("--------Server--------");
+		
+		//1. 指定Server端口 - 使用ServerSocket創建服務器
+		ServerSocket server = new ServerSocket(8888); //Throws
+		
+		//2. 阻塞式等待連接 accept - 創建Socket物件
+		Socket client = server.accept();
+		System.out.println("一個客戶端建立了連接");
+		
+		
+	}
 }
--- src/lili/com/chatroom/Client.java
+++ src/lili/com/chatroom/Client.java
@@ -1,5 +1,20 @@
 package lili.com.chatroom;
 
-public class Client {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+/**
+ * *在線聊天室 : Client 端
+ * @author LiLi-PC
+ *
+ */
+public class Client {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		System.out.println("--------Client--------");
+		
+		//1. 與Server端建立連接 - 使用Socket
+		Socket client = new Socket("localhost", 8888); //Throws
+	}
 }
